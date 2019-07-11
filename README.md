ff23002d5ac90e87f97c267d647dd5a2f61a95d0
Robert <zeng444@163.com>
2019-07-11 23:29:04 +0800

    初始化一个版本库第一版

---
+++ README.md
@@ -0,0 +1,174 @@
+# 中央储存服务
+
+## 背景
+
+图片存储作为基础服务，独立于各个应用外，通过远程调用发送图片到存储服务中。存储服务集中管理文件的创建，复制，删除以及CDN服务
+
+
+
+## 客户端程序
+
+### 安装
+
+```bash
+composer require janfish/storage 1.0 
+```
+
+
+### 注入phalcon
+
+
+```php
+$di->set('cloudStorage', function () use ($config) {
+    return new Janfish\Storage([
+        'api' => 'http://disk.xy.cn/',
+        'imagePrefix' => 'http://cdn.xy.cn/',
+        'tag' => 'insurance',
+        'appId' => 'test',
+        'appSecret' => 'test123456',
+    ]);
+});
+```
+
+- 参数配置说明
+
+
+| 参数       |  类型   |默认值 | 说明       |
+|------------|--------|--------|--------------|
+|api         | string |        | 服务地址http  |
+|appId       | string |        | 访问凭证ID   |
+|appSecret   | string |        | 访问凭密码   |
+|imagePrefix | string |        | CDN地址                         | 
+|pathType    | string | WEEK   | 生成日期文件的类型WEEK DAY MONTH|
+|tag         | string |        | 定义私有根目录，需要服务端开通可以使用的TAG    |
+|allowed     | array  |        | 允许上传的文件格式，数组默认图片的mime信息，需要小于等于服务端的设置 |
+|maxSize     | int    |        | 允许上传的文件大小,单位mb，需要小于服务端的设置  |
+|gzip        | boolean| true   | 传输开启gzip压缩  |
+
+DEV测试api参数为
+
+```
+api: http://disk.xy.cn/
+appId: test
+appSecret: test123456
+```
+
+### 调用上传
+
+#### 单文件上传
+
+```
+$result = $cloudStorage->setFile($_FILES[0]['tmp_name'])->upload();
+if ($result === false) {
+    return $app->apiResponse->error($cloudStorage->getError());
+}
+$images = $cloudStorage->getResult();
+```
+
+返回的数据
+
+
+```json
+[
+    {
+        "status": "200",
+        "path": "insurance/201928/2332727541.jpeg",
+        "url": "http://cdn.xy.cn/insurance/201928/2332727541.jpeg"
+    }
+]
+```
+
+#### 指定tag上传
+
+```
+$result = $cloudStorage->setFile($_FILES[0]['tmp_name'])->setTag('test')->upload();
+if ($result === false) {
+    return $app->apiResponse->error($cloudStorage->getError());
+}
+$images = $cloudStorage->getResult();
+```
+
+#### 批量上传
+```
+$cloudStorage = $app->cloudStorage;
+$files = array_column($_FILES, 'tmp_name');
+$cloudStorage->setFiles($files);
+$cloudStorage->setTag('insurance');
+if ($cloudStorage->upload() === false) {
+    echo $cloudStorage->getError();
+}
+$images = $cloudStorage->getResult();
+```
+
+返回的数据
+
+
+```json
+[
+    {
+        "status": "200",
+        "path": "insurance/201928/2332727541.jpeg",
+        "url": "http://cdn.xy.cn/insurance/201928/2332727541.jpeg"
+    },
+    {
+        "status": "200",
+        "path": "insurance/201928/106785628.jpeg",
+        "url": "http://cdn.xy.cn/insurance/201928/106785628.jpeg"
+    }
+]
+```
+
+本地约束限制设置（服务端针对appid也有限制，权限应该在服务端的限制之下）
+
+```
+$result = $app->cloudStorage->setAllowed([
+     'image/jpeg',
+     'image/jpg',
+     'image/png',
+     'application/zip',
+     'application/x-rar',
+     'application/x-zip-compressed',
+ ])
+ ->setMaxSize(5)
+ ->setFiles(array_column($_FILES, 'tmp_name'))
+ ->setTag('insurance')->upload();
+if ($result === false) {
+   echo $app->cloudStorage->getError()
+}
+```
+
+### 删除文件
+
+```php
+$cloudStorage = $app->cloudStorage;
+if ($cloudStorage->remove(['insurance/201928/106785628.jpeg','insurance/201928/2332727541.jpeg']) === false) {
+    echo  $cloudStorage->getError();
+}
+    
+```
+
+
+### 调用地址
+
+#### 获取CDN地址
+
+> 上传接口返回的path一般本地存取，通过拼装函数获得最终CDN地址
+
+
+```php
+echo $app->cloudStorage->getStaticUrl('insurance/201928/2332727541.jpeg');
+```
+
+输出数据
+
+```
+http://cdn.xy.cn/insurance/201928/2332727541.jpeg
+```
+
+#### 图片在线裁剪（需要CDN支持）
+
+```php
+echo $app->cloudStorage->getStaticUrl('insurance/201928/2332727541.jpeg',['width'=>100,'height'=>100,clipType='1']);
+```
+
+
---
+++ janfish/storage/src/Client.php
@@ -0,0 +1,429 @@
+<?php
+
+namespace Janfish\Storage;
+
+use Exception;
+
+/**
+ * Author:Robert Tsang
+ *
+ * Class Client
+ * @package Storage
+ */
+class Client
+{
+
+
+    /**
+     * Author:Robert
+     *
+     * @var mixed
+     */
+    protected $imagePrefix;
+
+    /**
+     * 接口地址http
+     * Author:Robert
+     *
+     * @var mixed|string
+     */
+    protected $api = '';
+
+    /**
+     * Author:Robert
+     *
+     * @var mixed|string
+     */
+    protected $appId = '';
+
+    /**
+     * Author:Robert
+     *
+     * @var bool|mixed
+     */
+    protected $gzip = true;
+
+    /**
+     * Author:Robert
+     *
+     * @var string
+     */
+    protected $appSecret = '';
+
+    /**
+     * Author:Robert
+     *
+     * @var mixed|string
+     */
+    protected $pathType = 'WEEK';
+
+
+    /**
+     * Author:Robert
+     *
+     * @var array
+     */
+    protected $files = [];
+
+
+    /**
+     * Author:Robert
+     *
+     * @var array
+     */
+    protected $response = [];
+
+    /**
+     * 项目标签
+     * Author:Robert
+     *
+     * @var mixed|string
+     */
+    protected $tag = 'default';
+
+    /**
+     * Author:Robert Tsang
+     *
+     * @var string
+     */
+    protected $error = '';
+
+    /**
+     * Author:Robert
+     *
+     * @var array
+     */
+    public static $pathTypeMap = ['DAY', 'WEEK', 'MONTH'];
+
+    /**
+     * Author:Robert
+     *
+     * @var array
+     */
+    protected $allowed = [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/jpg',
+        'image/png',
+        'image/x-png',
+    ];
+
+    /**
+     * Author:Robert
+     *
+     * @var int
+     */
+    protected $maxSize = 5;
+
+    const PATH_INFO = 'attachments';
+
+
+    /**
+     * Client constructor.
+     * @param array $options
+     * @throws Exception
+     */
+    public function __construct(array $options = [])
+    {
+        if (isset($options['api'])) {
+            $this->api = $options['api'];
+        }
+        if (isset($options['imagePrefix'])) {
+            $this->imagePrefix = $options['imagePrefix'];
+        }
+        if (isset($options['pathType'])) {
+            $this->pathType = $options['pathType'];
+        }
+        if (isset($options['tag'])) {
+            $this->tag = $options['tag'];
+        }
+        if (isset($options['appId'])) {
+            $this->appId = $options['appId'];
+        }
+        if (isset($options['gzip'])) {
+            $this->gzip = $options['gzip'];
+        }
+        if (isset($options['appSecret'])) {
+            $this->appSecret = $options['appSecret'];
+        }
+        if (isset($options['allowed'])) {
+            $this->allowed = $options['allowed'];
+        }
+        if (isset($options['maxSize'])) {
+            $this->maxSize = $options['maxSize'];
+        }
+        if (!$this->api) {
+            throw new Exception('对不起，请先配置文件存储服务');
+        }
+        if (!preg_match('/^\w+$/', $this->tag)) {
+            throw new Exception('对不起tag格式不合法');
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param array $allowed
+     * @return $this
+     */
+    public function setAllowed(array $allowed)
+    {
+        $this->allowed = $allowed;
+        return $this;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param int $size
+     * @return $this
+     */
+    public function setMaxSize(int $size)
+    {
+        $this->maxSize = $size;
+        return $this;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param string $tag
+     * @return $this
+     */
+    public function setTag(string $tag)
+    {
+        $this->tag = $tag;
+        return $this;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param string $file
+     * @return $this
+     * @throws Exception
+     */
+    public function setFile(string $file)
+    {
+        if (!$file || !file_exists($file)) {
+            throw new Exception('文件路径不存在，请检查'.$file, 500);
+        }
+        $this->files[] = [
+            'file' => $file,
+            'size' => filesize($file),
+            'mime' => mime_content_type($file),
+        ];
+        return $this;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param $files
+     * @return $this
+     * @throws Exception
+     */
+    public function setFiles($files)
+    {
+        $files = is_array($files) ? $files : [$files];
+        foreach ($files as $file) {
+            $this->setFile($file);
+        }
+        return $this;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function upload(): bool
+    {
+        foreach ($this->files as &$file) {
+            if (!$file || !file_exists($file['file'])) {
+                throw new Exception('文件路径不存在，请检查'.$file['file'], 500);
+            }
+            if (!in_array($file['mime'], $this->allowed)) {
+                $this->error = '不允许上传的文件格式类型:'.$file['mime'];
+                return false;
+            }
+            if ($file['size'] > $this->maxSize * 1048576) {
+                $this->error = '文件大小超过允许的最大值:'.$this->maxSize.'m';
+                return false;
+            }
+        }
+        $res = json_decode($this->http($this->getApiUrl(), 'POST', [
+            'time' => time(),
+            'tag' => $this->tag,
+            'pathType' => $this->pathType,
+        ], $this->files), true);
+        if (!$res) {
+            throw new Exception('数据的格式返回返回', 500);
+        }
+        if (!isset($res['status']) || $res['status'] !== 'success') {
+            $this->error = $res['message'] ?? '文件上传错误';
+            return false;
+        }
+        $this->response = $res['data']['attachments'] ?? [];
+        return true;
+    }
+
+    /**
+     * Author:Robert Tsang
+     *
+     * @param $files
+     * @return bool
+     * @throws Exception
+     */
+    public function remove($files): bool
+    {
+        $files = is_array($files) ? $files : [$files];
+        $res = json_decode($this->http($this->getApiUrl(), 'DELETE', ['files' => $files]), true);
+        if (!$res) {
+            throw new Exception('数据的格式返回返回', 500);
+        }
+        if (!isset($res['status']) || $res['status'] !== 'success') {
+            $this->error = $res['message'] ?? '删除文件失败';
+            return false;
+        }
+        $this->response = $res['data']['attachments'] ?? [];
+        return true;
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @return string
+     */
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @param string $file
+     * @param array $opt
+     * @return string
+     */
+    public function getStaticUrl(string $file = '', $opt = []): string
+    {
+        return $this->imagePrefix.'/'.$file;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param int $index
+     * @return int
+     */
+    public function getSize(int $index = 0): int
+    {
+        return $this->files[$index]['size'];
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @param int $index
+     * @return string
+     */
+    public function getMime(int $index = 0): string
+    {
+        return $this->files[$index]['mime'];
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @return string
+     */
+    public function makeBasicToken(): string
+    {
+        return "Authorization: Basic ".base64_encode(sprintf('%s:%s', $this->appId, $this->appSecret));
+    }
+
+
+    /**
+     * 地址
+     * Author:Robert
+     *
+     * @return string
+     */
+    protected function getApiUrl(): string
+    {
+        return sprintf('%s%s', $this->api, self::PATH_INFO);
+    }
+
+    /**
+     * Author:Robert Tsang1
+     *
+     * @param string $api
+     * @param string $method
+     * @param array $fields
+     * @param array $files
+     * @return mixed
+     * @throws Exception
+     */
+    public function http(string $api, string $method, array $fields = [], array $files = []): string
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_USERAGENT, "X.Y R&D Apollo Program");
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            $this->makeBasicToken(),
+        ]);
+        if (in_array($method, ['POST', 'PUT'])) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+        }
+        if (in_array($method, ['POST']) && $files) {
+            curl_setopt($curl, CURLOPT_SAFE_UPLOAD, 1);
+            foreach ($files as $index => $file) {
+                $fields["file$index"] = curl_file_create($file['file'], ($file['mime']));
+            }
+        }
+        if ($fields) {
+            if (in_array($method, ['PUT', 'POST'])) {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
+            } else {
+                $api .= '?'.http_build_query($fields);
+            }
+        }
+        curl_setopt($curl, CURLOPT_URL, $api);
+        if ($method) {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        }
+        if ($this->gzip === true) {
+            curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $res = curl_exec($curl);
+        if (curl_errno($curl)) {
+            throw new Exception(curl_error($curl), 0);
+        }
+        $httpStatusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (200 !== $httpStatusCode) {
+            throw new Exception($res, $httpStatusCode);
+        }
+        return $res;
+    }
+
+
+}
